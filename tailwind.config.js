a9817a9f89068fc954acbb0231a1517a37f1d9f1
Xuru Ren <xrventuresllc@gmail.com>
2025-09-08 15:15:20 -0700

    Tailwind fix + campus switcher live

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{ts,tsx}'],
-  theme: { extend: {} },
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}"   // <-- make sure .tsx is included (and friends)
+  ],
+  theme: {
+    extend: {},
+  },
   plugins: [],
 };
